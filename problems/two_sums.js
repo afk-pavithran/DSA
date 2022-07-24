19c8e850518749bdb6f326147d18ad3e5c0ae5e2
afk-pavithran <rupavithran@gmail.com>
2022-07-24 12:25:58 +0530

    two sums problem

---
+++ problems/two_sums.js
@@ -0,0 +1,16 @@
+
+const twoSums = (arr, sum)  => {
+    for (i=0; i<=arr.length; i++) {
+        for (j=i+1; j<=arr.length - 1; j++) {
+            if (arr[i] + arr[j] === sum) {
+                return [arr[i], arr[j]]
+            }
+        } 
+    }
+    return null
+}
+
+
+const listA = [1, 9, 2, 17, 5, 35, 4]
+sumNumbers = 40
+console.log(twoSums(listA, sumNumbers))
